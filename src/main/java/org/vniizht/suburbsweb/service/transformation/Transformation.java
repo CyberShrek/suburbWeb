edf01fff3be48da7bcd60c264888c932cfd6f808
CyberShrek <illych.jr2@gmail.com>
2024-05-16 17:03:09 +0300

    working on POJO

--- src/main/java/org/vniizht/suburbsweb/service/transformation/Transformation.java
+++ src/main/java/org/vniizht/suburbsweb/service/transformation/Transformation.java
@@ -43,11 +43,11 @@ public class Transformation {
 
         logger.log("\tЦО22");
         T1 t1 = createT1(l2Record, 1);
-        T2 t2 = createT2(l2Record, t1);
-        T3 t3 = createT3(l2Record, t1);
-        T4 t4 = createT4(l2Record, t1);
-        T5 t5 = createT5(l2Record, t1, t2, t3);
-        T6 t6 = createT6(l2Record, t1);
+        T2 t2 = createT2(l2Record,(short)1, t1);
+        T3 t3 = createT3(l2Record,(short)1, t1);
+        T4 t4 = createT4(l2Record,(short)1, t1);
+        T5 t5 = createT5(l2Record,          t1, t2, t3);
+        T6 t6 = createT6(l2Record,(short)1, t1);
 
         logger.log("\tЛьготные");
         Reestr reestr = createReestr(l2Record);
@@ -64,8 +64,8 @@ public class Transformation {
 
         // Станции
         String operationStation = l2.main.getSale_station(),
-                depStation = l2.main.getDeparture_station(),
-                arrStation = l2.main.getArrival_station();
+                     depStation = l2.main.getDeparture_station(),
+                     arrStation = l2.main.getArrival_station();
 
         // Флаги
         Character  so = l2.main.getFlg_so(),
@@ -80,7 +80,7 @@ public class Transformation {
                         webId = l2.main.getWeb_id();
         Character paymentType = l2.main.getPaymenttype(),
                 abonementType = l2.main.getAbonement_type().charAt(0),
-                  luggageType = l2.main.getCarryon_type(),
+                  carrionType = l2.main.getCarryon_type(),
                 trainCategory = l2.main.getTrain_category(),
                 carriageClass = l2.main.getCarriage_class();
 
@@ -89,7 +89,7 @@ public class Transformation {
                 .idnum(l2.main.getIdnum())
                 .request_num(l2.main.getRequest_num())
                 .date_zap(l2.main.getRequest_date())
-//                .yymm(Transformer.yyyymm2yymm(l2.main.getYyyymm()))
+                .yymm(Transformer.yyyymm2yymm(l2.main.getYyyymm()))
                 .p1("tab1")
                 .p2(serial)
                 .p3(Transformer.date2yyyy(operationDate))
@@ -110,7 +110,7 @@ public class Transformation {
                 .p18(nsiData.getArea(depStation, operationDate)) // !!
                 .p19(Transformer.interpretTrainCategory(trainCategory, '?')) // !! 2 и 3
                 .p20(Transformer.interpretCarriageClass(carriageClass))
-                .p21(Transformer.interpretTicketType('?', bag, '?', abonementType, twoWay, '?')) // Спросить Корзун про тип документа
+                .p21(Transformer.interpretTicketType(carrionType, abonementType, twoWay))
                 .p22(Transformer.interpretPassengerCategory(bsp, child, benefitCode))
                 .p23('3') // ?
                 .p24(benefitCode)
@@ -146,7 +146,7 @@ public class Transformation {
                 .p54(arrStation)
                 .p55(Transformer.interpretAbonementType(abonementType))
 //                .p56() жду справочник
-                .p57(Transformer.interpretLuggageType(luggageType))
+                .p57(Transformer.interpretCarrionType(carrionType))
                 .p58(so)
                 .p59(so)
                 .p60("???")
@@ -156,54 +156,88 @@ public class Transformation {
                 .build();
     }
 
-    private T2 createT2(Level2Data.Record record, T1 t1) {
+    private T2 createT2(Level2Data.Record l2, short serial, T1 t1) {
         logger.log("\t\tprig_co22_t2");
         return T2.builder()
                 .yymm(t1.getYymm())
                 .p1("tab2")
+                .p2(t1.getP5())
+                .p3(t1.getP2())
+                .p4(serial)
+//                .p5()  // берётся из функции
+//                .p6()  // берётся из функции
+//                .p7()  // берётся из функции
                 .build();
     }
 
-    private T3 createT3(Level2Data.Record record, T1 t1) {
+    private T3 createT3(Level2Data.Record l2, short serial, T1 t1) {
         logger.log("\t\tprig_co22_t3");
         return T3.builder()
                 .yymm(t1.getYymm())
                 .p1("tab3")
+                .p2(t1.getP5())
+                .p3(t1.getP2())
+                .p4(serial)
+//                .p5()  // берётся из функции
+//                .p6()  // берётся из функции
+//                .p7()  // берётся из функции
                 .build();
     }
 
-    private T4 createT4(Level2Data.Record record, T1 t1) {
+    private T4 createT4(Level2Data.Record l2, short serial, T1 t1) {
         logger.log("\t\tprig_co22_t4");
         return T4.builder()
                 .yymm(t1.getYymm())
                 .p1("tab4")
-
+                .p2(t1.getP5())
+                .p3(t1.getP2())
+                .p4(serial)
+//                .p5()  // берётся из функции
+//                .p6()  // берётся из функции
+//                .p7()  // берётся из функции
+//                .p8()  // берётся из функции
+//                .p9()  // берётся из функции
                 .build();
     }
 
-    private T5 createT5(Level2Data.Record record, T1 t1, T2 t2, T3 t3) {
+    private T5 createT5(Level2Data.Record l2, T1 t1, T2 t2, T3 t3) {
         logger.log("\t\tprig_co22_t5");
         return T5.builder()
                 .yymm(t1.getYymm())
                 .p1("tab5")
+                .p2(t1.getP5())
+//                .p3()
+//                .p4()
+//                .p5()
+                .p6(t1.getP33())
+                .p7(0L) // ??
+                .p8(0L) // ??
+                .p9(t1.getP36())
+                .p10(t1.getP44())
                 .build();
     }
 
-    private T6 createT6(Level2Data.Record record, T1 t1) {
+    private T6 createT6(Level2Data.Record l2, short serial, T1 t1) {
         logger.log("\t\tprig_co22_t6");
         return T6.builder()
                 .yymm(t1.getYymm())
                 .p1("tab6")
+                .p2(t1.getP5())
+                .p3(t1.getP2())
+                .p4(serial)
+//                .p5()  // берётся из функции
+//                .p6()  // берётся из функции
+//                .p7()  // берётся из функции
                 .build();
     }
     
-    private Reestr createReestr(Level2Data.Record record) {
+    private Reestr createReestr(Level2Data.Record l2) {
         logger.log("\t\tprig_lgot_reestr");
         return Reestr.builder()
                 .build();
     }
     
-    private Stat createStat(Level2Data.Record record) {
+    private Stat createStat(Level2Data.Record l2) {
         logger.log("\t\tprig_lgot_stat");
         return Stat.builder()
                 .build();
--- src/main/java/org/vniizht/suburbsweb/service/transformation/Transformer.java
+++ src/main/java/org/vniizht/suburbsweb/service/transformation/Transformer.java
@@ -33,15 +33,15 @@ public class Transformer {
     }
 
     public static Character interpretTrainCategory(Character category, Character prod) {
-        //        case.
-        // when train_category='С' then '6'
-        // when train_category='7' then '5'
-        // when train_category='А' then '8'
-        // when train_category='Г' then '9'
-        // when train_category='Л' then 'Л'
-        // when train_category='Б' then '7'
-        // when train_category in('1','М') then '4'
-        // when prod in('i','m') then '4'  else '1' end
+//                case.
+//         when train_category='С' then '6'
+//         when train_category='7' then '5'
+//         when train_category='А' then '8'
+//         when train_category='Г' then '9'
+//         when train_category='Л' then 'Л'
+//         when train_category='Б' then '7'
+//         when train_category in('1','М') then '4'
+//         when prod in('i','m') then '4'  else '1' end
         switch (category){
             case 'С': category = '6'; break;
             case '7': category = '5'; break;
@@ -60,37 +60,23 @@ public class Transformer {
         return "0" + carriageClass;
     }
 
-    public static Character interpretTicketType(Character sbor,
-                                                Character bag,
-                                                Character prod,
+    public static Character interpretTicketType(Character carrionType,
                                                 Character abonementType,
-                                                Character twoWay,
-                                                Character workDay) {
-        // case
-        // when flg_bil_sbor='S' then '8'
-        // when flg_bag='1' then '6'
-        // when prod in('i','m') then '1'
-        // when abonement_type='0' and flg_tuda_obr='2' then '3'
-        // when abonement_type='0' and flg_tuda_obr='1' then '2'
-        // when flg_rab_day='1' then '4'
-        // when flg_bag='0' then '5'  else '0' end
-        return   sbor == 'S' ? '8'
-                : bag == '1' ? '6'
-                : prod == 'i' || prod == 'm' ? '1'
-                : abonementType == '0' && twoWay == '2' ? '3'
-                : abonementType == '0' && twoWay == '1' ? '2'
-                : workDay == '1' ? '4'
-                : bag == '0' ? '5'
-                : '0';
+                                                Character twoWay) {
+        if(abonementType == '0') {
+            if(carrionType == ' ')
+                return (twoWay == '1') ? '3' : '2';
+            else
+                return '6';
+        }
+
+
+        return '0';
     }
 
     public static Character interpretPassengerCategory(Character bsp,
                                                        Character child,
                                                        String benefitCode) {
-        // case
-        // when flg_bsp='1' then '4'
-        // when flg_child='1' then '2'
-        // when kod_lgt=0 then '1' else '3' end
         return    bsp   == '1' ? '4'
                 : child == '1' ? '2'
                 : benefitCode.equals("0") ? '1'
@@ -108,10 +94,6 @@ public class Transformer {
                                                      String tsite,
                                                      short requestType,
                                                      short requestSubtype) {
-        // case when flg_ruch='0' then '1'
-        // when tsite is not null then cast(cast(tsite as smallint) as char(1))
-        // when request_subtype in(10,20,25) then '4'
-        // when request_type!=64 and request_subtype>=200 and request_subtype<=299 then '2' else '5' end
         return flgRuch == '0' ? '1'
                 : tsite != null ? tsite.charAt(0)
                 : requestSubtype == 10 || requestSubtype == 20 || requestSubtype == 25 ? '4'
@@ -120,13 +102,6 @@ public class Transformer {
     }
 
     public static Character interpretAbonementType(Character abonementType) {
-        // case when ABONEMENT_TYPE='3' then '1'
-        // when ABONEMENT_TYPE in('5','6') then '2'
-        //  when ABONEMENT_TYPE in('7','8') then '3'
-        // when ABONEMENT_TYPE='4' then '1'
-        // when ABONEMENT_TYPE='1' then '5'
-        // when ABONEMENT_TYPE='2' then '4'
-        // else ABONEMENT_TYPE end
         return abonementType == '3' ? '1'
                 : abonementType == '5' || abonementType == '6' ? '2'
                 : abonementType == '7' || abonementType == '8' ? '3'
@@ -140,32 +115,19 @@ public class Transformer {
                                                     short srok_mon,
                                                     k_pas,
                                                     srok_bil) {
-        // case
-        // when srok_mon>0 and srok_mon<10 then '00'||cast(srok_mon as char(1))
-        // when srok_mon>9 then '0'||cast(srok_mon as char(12))
-        // when ABONEMENT_TYPE='1' then '4'||cast(k_pas*2 as char(2))
-        // when ABONEMENT_TYPE>'1' and srok_bil<10 then '10'||cast(srok_bil as char(2))
-        // when ABONEMENT_TYPE>'1' then '1'||cast(srok_bil as char(2))
-        // else '000' end
         return abonementType == '1' ? '4' + k_pas * 2
                 : abonementType > '1' && srok_bil < 10 ? '10' + srok_bil
                 : abonementType > '1' ? '1' + srok_bil
                 : "000";
     }
 
-    public static Character interpretLuggageType(Character carriageType){
-        // case
-        // when bag_vid='Ж' then '1'
-        // when bag_vid='Т' then '2'
-        // when bag_vid='В' then '3'
-        // when bag_vid='Р' then '4'
-        // else  bag_vid end
-        switch (carriageType){
-            case 'Ж': carriageType = '1'; break;
-            case 'Т': carriageType = '2'; break;
-            case 'В': carriageType = '3'; break;
-            case 'Р': carriageType = '4';
+    public static Character interpretCarrionType(Character carrionType){
+        switch (carrionType){
+            case 'Ж': carrionType = '1'; break;
+            case 'Т': carrionType = '2'; break;
+            case 'В': carrionType = '3'; break;
+            case 'Р': carrionType = '4';
         }
-        return carriageType;
+        return carrionType;
     }
 }
