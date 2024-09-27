b527291556a35c7853bf1a373394ddd2070c0a62
CyberShrek <illych.jr2@gmail.com>
2024-09-27 18:07:46 +0300

    testing

---
+++ src/main/java/Playground.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playground {
+
+    public static void main(String[] args) {
+        int int1 = 1;
+        int int5 = 5;
+        System.out.println( 1 /(float) 5);
+
+    }
+}
